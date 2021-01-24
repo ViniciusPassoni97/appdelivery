e4d3fca5435197ab9018db08b01d8a89f30c31be
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-01-23 21:13:30 -0300

    Posição de ContainerSplash.footer em src/pages/splash

--- src/pages/splash/index.js
+++ src/pages/splash/index.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View,Image } from 'react-native';
+import { Text, View, ImageBackground } from 'react-native';
 import { ContainerSplash } from './styles';
-import SplashImage from '../../../assets/splashscreen.png'
+import SplashImage from '../../../assets/splashscreen.png';
 
 export default function Splash() {
   return (
     <View style={ContainerSplash.container}>
-      <Image source={SplashImage} style={ContainerSplash.srcImage}/>
-      <Text>Splashh</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={SplashImage} style={ContainerSplash.srcImage}>
+        <View style={ContainerSplash.footer}>
+
+        </View>
+      </ImageBackground>
     </View>
   );
 }
--- src/pages/splash/styles.js
+++ src/pages/splash/styles.js
@@ -2,10 +2,22 @@ import { StyleSheet } from 'react-native';
 
 export const ContainerSplash = StyleSheet.create({
     container:{
-        backgroundColor:'#f2f2f2',
+        flex:1,
+        width:'100%',
+        height:'100%',
+    },
+    containerDivImg:{
     },
     srcImage: {
-        flex: 1,
-        resizeMode: 'cover'
+        flex:1,
+        width:'100%',
+        height:'100%',
+    },
+    footer: {
+        height:'60%',
+        marginTop:'80%',
+        backgroundColor:'#f3f3f3',
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40
     }
 })
