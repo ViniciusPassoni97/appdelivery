910482e2d8730b81d8cb43ea80e023baecf6fd58
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-02-02 22:48:18 -0300

    Criando Header em src/pages/item

--- src/pages/item/index.js
+++ src/pages/item/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View,Text,Image,TextInput} from 'react-native';
 import { StyleCategories } from './style';
+import {RectButton} from 'react-native-gesture-handler';
+import Vector from '../../../assets/vector.png';
+
 
 export default function Item(){
     return(
         <View style={StyleCategories.container}>
-            <Text>SADSDASD</Text>
+            <View style={StyleCategories.containerItensHeader}>
+                <RectButton>
+                    <Image source={Vector} style={StyleCategories.containerItensHeaderImg}/>
+                </RectButton>
+                <Text style={StyleCategories.containerItensHeaderText} >Vegetables</Text>
+                <TextInput
+                    style={StyleCategories.containerItemHeaderInput}
+                />
+           </View>
         </View>
     )
 }
--- src/pages/item/style.js
+++ src/pages/item/style.js
@@ -2,6 +2,33 @@ import {StyleSheet} from 'react-native';
 
 export const StyleCategories = StyleSheet.create({
     container:{
-        backgroundColor:'#bcbcbc'
-    }
+        marginTop:50,
+        height: '100%',
+    },
+    containerItensHeader:{
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 1,
+        padding:20,
+        height:'25%',
+    },
+    containerItensHeaderImg:{
+        width:40,
+        height:40
+    },
+    containerItensHeaderText:{
+        marginTop:20,
+        fontSize:30,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    containerItemHeaderInput:{
+        height: 55, 
+        borderColor: 'gray', 
+        borderWidth: 1,
+        marginTop:15,
+        fontSize:20,
+        borderRadius:20,
+        padding:15
+    },
 });
