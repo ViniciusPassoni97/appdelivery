95afe99fce082928de5bbbc945f04c11d679aa94
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-03-01 22:49:21 -0300

    Melhorando estrutura e styles em src/pages/check

--- src/pages/check/index.js
+++ src/pages/check/index.js
@@ -1,9 +1,63 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View,Text,ScrollView} from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; 
+import {ContainerCheck} from './style';
+import {RectButton} from 'react-native-gesture-handler';
+import { EvilIcons,FontAwesome5 } from '@expo/vector-icons'; 
+import MapView from 'react-native-maps';
 
-export default function(){
+
+export default function Check(){
     return (
-        <View>
-        </View>
+        <ScrollView style={ContainerCheck.container}>
+            <View style={ContainerCheck.containerHeader}>
+                <RectButton>
+                    <Ionicons name="return-up-back" size={40} color="black" />
+                </RectButton>
+                <Text style={ContainerCheck.containerHeaderText}>Confira as Informações</Text>
+            </View>
+            <View>
+                <View style={ContainerCheck.containerPg}>
+                    <View style={ContainerCheck.containerPgText}>
+                        <Text style={ContainerCheck.containerPgMethod}>Método de Pagamento</Text>
+                        <RectButton>
+                            <Text>Editar</Text>
+                        </RectButton>
+                    </View>
+                    <View style={ContainerCheck.containerPgText}>
+                        <EvilIcons name="credit-card" size={24} color="black" />
+                        <Text>4747 4747 4747 4747</Text>
+                    </View>
+                </View>
+                <View style={ContainerCheck.containerPg}> 
+                    <View style={ContainerCheck.containerPgText}>
+                        <Text style={ContainerCheck.containerPgMethod}>Endereço para entrega</Text>
+                        <FontAwesome5 name="address-card" size={24} color="black" />
+                    </View>
+                    <View style={ContainerCheck.containerPgText}>
+                        <View style={ContainerCheck.containerMap}>
+                            <MapView
+                                style={ContainerCheck.containerHeaderMap}
+                                initialRegion={{
+                                latitude: 37.78825,
+                                longitude: -122.4324,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421,
+                            }}
+                        />
+            </View>
+                    </View>
+                </View>
+            </View>
+            <View>
+                
+            </View>
+            <View>
+                
+            </View>
+            <View>
+                
+            </View>
+        </ScrollView>
     );
 }
---
+++ src/pages/check/style.js
@@ -0,0 +1,58 @@
+import {StyleSheet} from 'react-native';
+
+export const ContainerCheck = StyleSheet.create({
+    container:{
+        flex: 1,
+        marginTop:'8%',
+        width:'100%',
+        height:'100%',
+        flexDirection: 'column'
+    },    
+    containerHeader:{
+        display: 'flex',
+        flexDirection: 'row',
+        width:'100%',
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop:15,
+        backgroundColor:'#f1f1f1',
+        alignItems: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 9,
+        },
+        shadowOpacity: 0.48,
+        shadowRadius: 11.95,
+        elevation: 18,
+        padding:10
+    },
+    containerHeaderText:{
+        fontWeight:'bold',
+        fontSize:20,
+        marginRight:'15%',
+        fontWeight:'bold'
+    },
+    containerPgMethod:{
+        fontWeight:'bold',
+        fontSize:16
+    },
+    containerPg:{
+        padding:15
+    },
+    containerPgText:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        marginTop:20,
+    },
+    containerMap:{
+        width: '100%',
+        height: 400
+    },
+    containerHeaderMap:{
+        width: '100%',
+        height: '100%'
+    },
+})
